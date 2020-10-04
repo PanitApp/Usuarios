78c0a1db6bc73665126ddb6748c26c5f1172f12d
daniel19066 <dpallaresr@unal.edu.co>
2020-10-04 09:54:19 -0500

    filtra por nombre de usuario

--- usuarios/views/usuario_view.py
+++ usuarios/views/usuario_view.py
@@ -19,6 +19,17 @@ class UsuarioList(mixins.ListModelMixin,
     def post(self, request, *args, **kwargs):
         return self.create(request, *args, **kwargs)
 
+    def get_queryset(self):
+        """
+        Optionally restricts the returned purchases to a given user,
+        by filtering against a `username` query parameter in the URL.
+        """
+        queryset = Usuario.objects.all()
+        nombre_usuario = self.request.query_params.get('nombre_usuario', None)
+        if nombre_usuario is not None:
+            queryset = queryset.filter(nombre_usuario=nombre_usuario)
+        return queryset
+
 
 class UsuarioDetail(mixins.RetrieveModelMixin,
                     mixins.UpdateModelMixin,
