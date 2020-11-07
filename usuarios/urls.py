e8a9c43c47b9820139f0f698cc65ff10a76f7e4f
Farid <difcortesgu@unal.edu.co>
2020-11-07 16:20:55 -0500

    conecta con el ldap pero falta poder autenticar

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -4,12 +4,14 @@ from usuarios.views.rol_view import RolList
 from usuarios.views.rol_view import RolDetail
 from usuarios.views.usuario_view import UsuarioList
 from usuarios.views.usuario_view import UsuarioDetail
+from usuarios.views.usuario_view import UsuarioLogin
 
 urlpatterns = [
     path('roles/', RolList.as_view()),
     path('roles/<int:pk>', RolDetail.as_view()),
     path('usuarios/', UsuarioList.as_view()),
     path('usuarios/<int:pk>', UsuarioDetail.as_view()),
+    path('login/<int:pk>', UsuarioLogin.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
--- usuarios/views/usuario_view.py
+++ usuarios/views/usuario_view.py
@@ -5,6 +5,8 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework import mixins
 from rest_framework import generics
+import ldap
+import os
 
 class UsuarioList(mixins.ListModelMixin,
                   mixins.CreateModelMixin,
@@ -47,3 +49,32 @@ class UsuarioDetail(mixins.RetrieveModelMixin,
 
     def delete(self, request, *args, **kwargs):
         return self.destroy(request, *args, **kwargs)
+
+class UsuarioLogin(mixins.RetrieveModelMixin,
+                   generics.GenericAPIView):
+
+    queryset = Usuario.objects.all()
+    serializer_class = UsuarioSerializer
+
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+
+        ldap_uri = os.environ.get('LDAP_URI')
+        
+        user = "uid=" + "difcortesgu" + "ou=users,dc=panitapp,dc=co"
+        password = "admin"
+
+        cnx = ldap.initialize(ldap_uri, bytes_mode=False)
+
+        try:
+            cnx.bind_s(user, password)
+            print("Sirvio!!!!!")
+        except ldap.SERVER_DOWN:
+            print('LDAP server is not reachable')
+        except ldap.INVALID_CREDENTIALS:
+            print('Invalid LDAP credentials')
+
+        return self.get(request, *args, **kwargs)
+
