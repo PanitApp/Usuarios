8e902058b2bad7a9e1bd2f7d3c480f17a3ee36dd
daniel19066 <dpallaresr@unal.edu.co>
2020-11-08 21:00:48 -0500

    sadas

--- project/urls.py
+++ project/urls.py
@@ -4,6 +4,5 @@ from django.contrib import admin
 urlpatterns = [
     path('', include('usuarios.urls')),
     path('admin/', admin.site.urls), 
-    path('api/', include('authentication.urls'))
 ]
 
