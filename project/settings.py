7427a2d68f16de96d4a3d5adad307bb22ee7a884
daniel19066 <dpallaresr@unal.edu.co>
2020-11-08 22:29:07 -0500

    sdad

--- project/settings.py
+++ project/settings.py
@@ -41,7 +41,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'usuarios.apps.usuariosConfig'
-    'rest_framework_simplejwt.token_blacklist'
 ]
 
 MIDDLEWARE = [
