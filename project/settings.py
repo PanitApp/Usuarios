0d13ac774f8c0a725cf452088e9337ca91fff7d9
daniel19066 <dpallaresr@unal.edu.co>
2020-11-11 10:46:08 -0500

    cambio a semanas duracion token

--- project/settings.py
+++ project/settings.py
@@ -143,8 +143,8 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=30),
-    'REFRESH_TOKEN_LIFETIME': timedelta(minutes=60),
+    'ACCESS_TOKEN_LIFETIME': timedelta(weeks=1),
+    'REFRESH_TOKEN_LIFETIME': timedelta(weeks=2),
     'ROTATE_REFRESH_TOKENS': True,
     'BLACKLIST_AFTER_ROTATION': True,
     'ALGORITHM': 'HS256',
